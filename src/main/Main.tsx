f5f2a6c91b263176812f17de88f2a3a03e64297d
Молодец 2705 <ant_papa@mail.ru>
2023-07-11 16:34:38 +0600

    main starts

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import s from './Main.module.css';
+import sContainer from '../common/styles/Container.module.css'
 
 
 function Main() {
-  return (
-    <div className={s.mainBlock}>
-
-    </div>
-  );
+    return (
+        <div className={s.mainBlock}>
+            <div className={sContainer.container}>
+                <div className={s.greeting}>
+                    <span>Hi There</span>
+                    <h1>I am Anton Martynenko</h1>
+                    <p>Frontend Developer</p>
+                </div>
+                <div className={s.photo}></div>
+            </div>
+        </div>
+    );
 }
 
 export default Main;
